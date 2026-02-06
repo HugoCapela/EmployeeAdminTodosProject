a05b5dd3b36a9f7085d886a1396198b414d8412e
HugoCapela <hugodrcapela@gmail.com>
2026-02-06 16:38:59 +0000

    Lesson 30 - Find Authentication User

--- src/main/java/com/luv2code/springboot/todos/service/UserServiceImpl.java
+++ src/main/java/com/luv2code/springboot/todos/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.luv2code.springboot.todos.entity.Authority;
 import com.luv2code.springboot.todos.entity.User;
 import com.luv2code.springboot.todos.repository.UserRepository;
 import com.luv2code.springboot.todos.response.UserResponse;
+import com.luv2code.springboot.todos.util.FindAuthenticatedUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
@@ -16,21 +17,17 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final FindAuthenticatedUser findAuthenticatedUser;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, FindAuthenticatedUser findAuthenticatedUser) {
         this.userRepository = userRepository;
+        this.findAuthenticatedUser = findAuthenticatedUser;
     }
 
     @Override
     @Transactional(readOnly = true)
     public UserResponse getUserInfo() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() ||
-                authentication.getPrincipal().equals("anonymousUser")) {
-            throw new AccessDeniedException("Authentication is required");
-        }
-
-        User user = (User) authentication.getPrincipal();
+        User user = findAuthenticatedUser.getAuthenticatedUser();
 
         return new UserResponse(
                 user.getId(),
@@ -42,13 +39,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated() ||
-                authentication.getPrincipal().equals("anonymousUser")) {
-            throw new AccessDeniedException("Authentication is required");
-        }
-
-        User user = (User) authentication.getPrincipal();
+        User user = findAuthenticatedUser.getAuthenticatedUser();
 
         if (isLastAdmin(user)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Admin cannot delete itself");
